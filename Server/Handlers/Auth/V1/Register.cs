e9f33aa9223d75903319fe4948ee98698655fbd2
Logan Dang <76602370+Logan-Dang@users.noreply.github.com>
2026-01-26 10:19:25 -0800

    mock cognito (#6)
    * mock cognito in development

--- Server/Handlers/Auth/V1/Register.cs
+++ Server/Handlers/Auth/V1/Register.cs
@@ -1,55 +1,27 @@
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using Amazon.CognitoIdentityProvider;
-using Amazon.CognitoIdentityProvider.Model;
+using Server.Application.Auth;
 
 namespace Server.Handlers.Auth.V1;
 
 public static class Register
 {
-  public static async Task<IResult> Handle(Request request, AmazonCognitoIdentityProviderClient client)
+  public static async Task<IResult> Handle(RegisterRequest request, ICognitoService cognito)
   {
-    var cognitoClientId = Environment.GetEnvironmentVariable("COGNITO_CLIENT_ID");
-    if (string.IsNullOrEmpty(cognitoClientId))
-    {
-      Debug.WriteLine("Cognito Client ID is not configured.");
-      return Results.Problem();
-    }
+    var response = await cognito.RegisterAsync(request);
 
-    var attributes = new List<AttributeType>
-    {
-      new() {
-        Name = "email",
-        Value = request.Email
-      },
-      new() {
-        Name = "name",
-        Value = request.FullName
-      }
-    };
-
-    var signUpRequest = new SignUpRequest
-    {
-      ClientId = cognitoClientId,
-      Username = request.Email,
-      Password = request.Password,
-      UserAttributes = attributes
-    };
-
-    var response = await client.SignUpAsync(signUpRequest);
-
-    return Results.Created($"/auth/v1/users/{response.UserSub}", new Response(
+    return Results.Created($"/auth/v1/users/{response.UserSub}", new RegisterResponse(
       Id: response.UserSub,
       Email: request.Email,
       FullName: request.FullName
     ));
   }
-  public record Request(
-    [Required, EmailAddress]
-    string Email,
-    [Required, MinLength(8)]
-    string Password,
-    [Required]
-    string FullName);
-  public record Response(string Id, string Email, string FullName);
 }
+public record RegisterRequest(
+  [Required, EmailAddress]
+    string Email,
+  [Required, MinLength(8)]
+    string Password,
+  [Required]
+    string FullName);
+
+public record RegisterResponse(string Id, string Email, string FullName);
